ebee9bf846eb3d130e39683b720a135d59c40c67
Abdul Sabir <sabirold91@gmail.com>
2018-07-24 10:52:16 +0500

    No changes, just commited for pushing

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', (blah) => (blah + new Date().getFullYear()));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to Home page!</h1>');
+});
+
+app.get('/json', (req, res) => {
+    res.send({
+        name: "sabir",
+        age: 20,
+        occupation: "student",
+        interest: "computer science",
+        anotherObject: {
+            name: "sabir",
+            age: 20,
+            occupation: "student",
+            interest: "computer science"
+        }
+    })
+
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        heading1: 'About Page',
+        footer: 'Copyright 2016'
+    });
+})
+
+app.get('/some', (req, res) => {
+    res.render('some.hbs', {
+        heading1: 'About Page',
+        footer: 'Copyright 2016'
+    });
+})
+
+app.get('/other', (req, res) => {
+    res.render('other.hbs', {
+        heading1: 'About Page',
+        footer: 'Copyright 2016'
+    });
+})
+app.listen(8080, () => {
+    console.log("Server is running at http://localhost:8080");
+});
